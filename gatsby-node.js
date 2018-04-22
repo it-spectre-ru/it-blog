d236cf33c9e331bbea4e1d1c5179c7696c4ad969
it-spectre-ru <it.spectre.ru@gmail.com>
2018-04-22 22:41:55 +0500

    fix error mistype

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,37 @@
-const path = require('path');
-
+const path = require('path')
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-	const { createPage } = boundActionCreators
+  const { createPage } = boundActionCreators
 
-	const postTemplate = path.resolve('src/temlates/blog/blog-post.js');
+  const postTemplate = path.resolve('src/templates/blog-post.js')
 
-	return graphql(`
-		{
-			allMarkdownRemark {
+  return graphql(`
+    {
+      allMarkdownRemark {
         edges {
-            node {
-								html
-                id
-                frontmatter {
-                    path
-                    title
-                    date
-                    author
-                }
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+              date
+              author
             }
+          }
         }
+      }
     }
-		}
-	`).then(res => {
-			if (res.errors) {
-				return Promise.reject(res.errors)
-			}
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
 
-			res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-				createPage({
-					path: node.frontmatter.path,
-					component: postTemplate
-				})
-			})
-		})
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+      })
+    })
+  })
 }
