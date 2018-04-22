0bf5482f52982e212e63ffdbc8772c57e2bc7f22
it-spectre-ru <it.spectre.ru@gmail.com>
2018-04-22 14:55:43 +0500

    add menu

---
+++ src/pages/services.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ServicesPage = () => (
+    <div>
+      <h1>Services </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aperiam
+        at, dolorum harum molestiae natus necessitatibus nesciunt odio quia
+        sapiente sint ullam vero voluptate. A accusantium adipisci autem beatae
+        eos facilis fugit harum id, laudantium molestias natus nemo non
+        temporibus!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aperiam
+        at, dolorum harum molestiae natus necessitatibus nesciunt odio quia
+        sapiente sint ullam vero voluptate. A accusantium adipisci autem beatae
+        eos facilis fugit harum id, laudantium molestias natus nemo non
+        temporibus!</p>
+    </div>
+);
+
+export default ServicesPage;
